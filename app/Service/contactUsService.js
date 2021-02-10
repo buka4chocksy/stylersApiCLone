b4a6fc8fa83ded47f5020202fe8e8da976992d02
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-02-10 15:29:30 +0100

    added subscribers method

--- app/Service/contactUsService.js
+++ app/Service/contactUsService.js
@@ -1,6 +1,9 @@
 var model = require('../Model/contactUs');
 var user = require('../Model/user');
 var mailer = require('../Middleware/mailer');
+var subscribers = require('../Model/subscribers')
+const Excel = require('exceljs');
+const xl = require('excel4node');
 exports.createHelp = (data) => {
     return new Promise((resolve, reject) => {
         user.findById({ _id: data.userId }).then(found => {
@@ -19,4 +22,33 @@ exports.createHelp = (data) => {
             resolve({ success: false, message: err });
         });
     })
+}
+
+exports.addSubscribers = async (data) => {
+    try {
+        let checkemail = await subscribers.findOne({ email: data.email })
+        if (checkemail) {
+            return { success: false, message: 'you already subscribed to our news letter' }
+        } else {
+              let result = {email:data.email}
+              await  subscribers.create(result)
+             await mailer.subscriberNoti(data.email )
+            return { success: true, message: 'subscription was done successfully' }
+        }
+    } catch (err) {
+        return err
+    }
+}
+
+exports.deleteSubscriber = async (data) => {
+    try {
+        let deleted = await subscribers.findOneAndDelete({ email: data.email })
+        if (deleted) {
+            return { success: false, message: 'subscriber deleted' }
+        } else {
+            return { success: true, message: 'unable to delete subscriber ' }
+        }
+    } catch (err) {
+        return err
+    }
 }
