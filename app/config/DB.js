10753950fad9e5d5d7f0175827b621de94aafe9e
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-06-23 12:45:50 +0100

    changes

--- app/config/DB.js
+++ app/config/DB.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
-
 //const uri = process.env.DB_URI;
-const uri = process.env.NODE_ENV == 'prod' ? process.env.DB_PROD : process.env.DB_LOCAL;
+const uri = process.env.DB_PROD ;
 
 module.exports = function init() {
     if (uri) {
